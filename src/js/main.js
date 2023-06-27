cf459ba65c17fdd73ac7e94c2c2d73f9f2788bc5
Victor <toloknovv1gmail.com>
2023-06-27 20:05:18 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,21 @@
+// import AOS from "aos"; // если эта библотека не надо то удалить ее
+// AOS.init();
+// //getE
+import { info, cardsPath } from "./db";
+import { swiperJs } from "./slider";
+import { formValidation } from "./validator";
+import { modal } from "./modal";
+import { customSelector } from "./customSelect";
+import { burgerMenu } from "./burgerMenu";
+import { anchors } from "./anchors";
+
+window.addEventListener("DOMContentLoaded", () => {
+  // swiperJs();
+  // servicesWindow();
+  // burgerMenu();
+  // showAssortment();
+  // customSelector();
+  // modal();
+  // anchors();
+  // formValidation();
+})
